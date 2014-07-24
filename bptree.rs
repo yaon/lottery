8be4da76c6e8f77cff8a82abe574a5141f4dd8b7
Jim <caillardjim@gmail.com>
2014-07-24 12:58:34 -0400

    Init

---
+++ bptree.rs
@@ -0,0 +1,57 @@
+// Max number of keys per node
+static ORDER: uint = 4;
+
+enum BPTree<Key, Value>
+{
+  Node(
+    uint,                                     // Branches
+    [Key, ..ORDER],                           // Keys
+    [Option<BPTree<Key, Value>>, ..ORDER+1],  // Sons
+    Option<BPTree<Key, Value>>                // Father
+  ),
+  Leaf(
+    Key,
+    Value,
+    Option<BPTree<Key, Value>> // Father
+  ),
+}
+
+fn print<Key, Value>(tree: BPTree<Key, Value>)
+{
+}
+
+
+fn compare<Key>(a:Key, b:Key) -> int
+{
+  return -1;
+}
+
+fn search<Key, Value>(tree: &BPTree<Key, Value>, key: Key) -> int
+{
+  // loop {
+    match tree {
+      Node(b, k, s, _) => {
+        for i in range(1u, b) {
+          if compare(k[1], key) < 0 {
+          }
+        }
+        0
+      },
+      Leaf(_, v, _) => 0
+    };
+    return 0;
+  // }
+}
+
+fn insert<Key, Value>(tree: BPTree<Key, Value>, key: Key, value: Value)
+{
+}
+
+fn delete<Key, Value>(tree: BPTree<Key, Value>, key: Key)
+{
+}
+
+fn main()
+{
+  0;
+}
