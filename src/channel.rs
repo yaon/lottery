db3a00de6827e1436f444a6a963609c1b0c380bc
Jim <caillardjim@gmail.com>
2014-07-27 11:58:11 -0400

    Multiple workers sharing rx mutex + debug

--- src/channel.rs
+++ src/channel.rs
@@ -1,18 +1,32 @@
+#![feature(phase)]
+#[phase(plugin, link)] extern crate log;
+
+use std::sync::{Mutex, Arc};
 mod test;
+
+
+static nprocs : uint = 4u;
 
 struct Worker {
   tx: Sender<int>,
-  rx: Receiver<int>,
+  rx_mutex: Arc<Mutex<Receiver<int>>>,
 }
 
 impl Worker {
-  fn new(tx: Sender<int>, rx: Receiver<int>) -> Worker {
-    Worker { tx: tx, rx: rx }
+  fn new(tx: Sender<int>, rx_mutex: Arc<Mutex<Receiver<int>>>) -> Worker {
+    Worker { tx: tx, rx_mutex: rx_mutex }
   }
   fn start(&self) {
     loop {
-      let msg = self.rx.recv();
+      let mut msg;
+      {
+        let mut rx = self.rx_mutex.lock();
+        debug!("Worker: Got mutex");
+        msg = rx.recv();
+        debug!("Worker: Received {}", msg);
+      }
       long_computation();
+      debug!("Worker: Sending {}", msg);
       self.tx.send(msg);
     }
   }
@@ -21,12 +35,20 @@ impl Worker {
 fn main() {
   let (worker_tx, io_rx): (Sender<int>, Receiver<int>) = channel();
   let (io_tx, worker_rx): (Sender<int>, Receiver<int>) = channel();
+  let worker_rx_mutex = Arc::new(Mutex::new(worker_rx));
 
-  spawn(proc() {
-    let worker: Worker = Worker::new(worker_tx, worker_rx);
-    worker.start();
+  for _ in range(0u, nprocs) {
+    let worker_rx_mutex_clone = worker_rx_mutex.clone();
+    let worker_tx_clone = worker_tx.clone();
+
+    debug!("Spawning worker thread");
+    spawn(proc() {
+      let worker: Worker = Worker::new(worker_tx_clone, worker_rx_mutex_clone);
+      worker.start();
     });
+  }
 
+  debug!("Spawning tester thread");
   let tester: test::Tester = test::Tester::new(io_tx, io_rx);
   tester.start();
 
